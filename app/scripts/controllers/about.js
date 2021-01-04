745b2dea8885e89bed583661b0da6f380374681c
OSE_Uwindsor <oseuwindsor@gmail.com>
2021-01-04 16:25:22 -0500

    adding build files

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name designtoolApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the designtoolApp
+ */
+angular.module('designtoolApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name designtoolApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the designtoolApp
+ */
+angular.module('designtoolApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
